6a2402d18e4e730bec8cf79ac9a851dfae2fa1ac
Alexey <alexey@MacBook-Air.local>
2021-07-13 18:45:03 +0300

    update title and description meta, add h1 and project description

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+    return (
+        <h1 className="title">
+            Online ABI Encoding Service
+        </h1>
+    )
+}
+
+export default Title;
