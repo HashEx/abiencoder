4ad092958f9095ddc3608e6238f4650ba9ac53f8
Alexey <grimzest@gmail.com>
2022-04-10 15:25:47 +0300

    update info text

--- src/sections/InfoSection/index.tsx
+++ src/sections/InfoSection/index.tsx
@@ -5,7 +5,7 @@ import Section from '../../components/Section';
 const InfoSection = () => (
     <Section id="what-is-this" title="What is this?">
         <p>
-            When you want to <span className="green">verify</span>  your Solidity Smart Contract's code on <span className="green">Etherscan.io</span>  or manually make a transaction to call your contract's method, you need to encode input arguments in a special way.<br />
+            When you want to <span className="green">verify</span>  your Solidity Smart Contract's code on <span className="green">etherscan, bscscan, polygonscan and etc.</span>  or manually make a transaction to call your contract's method, you need to encode input arguments in a special way.<br />
             ABI Encoding Service helps you to encode these arguments in a such way.
         </p>
     </Section>
