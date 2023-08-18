7e8a9d61f103d1014fc909c6c090a91ecf422ab0
Alexey <alexey@MacBook-Pro.local>
2023-08-18 12:28:33 +0300

    optimize banners

--- src/constants/banners.ts
+++ src/constants/banners.ts
@@ -1,12 +1,12 @@
 import { DSA_URL, MAXIMIZER_URL, HASHEX_URL, TELEGRAM_BOT_URL } from "../constants/links";
-import telegramBot from "../images/telegramBot.svg";
+import telegramBot from "../images/telegramBot.jpeg";
 import telegramBotMob from "../images/telegramBotMob.svg";
-import dsa from "../images/dsa.svg";
+import dsa from "../images/dsa.jpg";
 import dsaMob from "../images/dsaMob.svg";
-import maximizer from "../images/maximizer.svg";
+import maximizer from "../images/maximizer.jpeg";
 import maximizerMob from "../images/maximizerMob.svg";
-import hashex from "../images/HashExBanner.jpg";
-import hashexMob from "../images/HashExBannerMob.jpg";
+import hashex from "../images/HashExBanner.jpeg";
+import hashexMob from "../images/HashExBannerMob.jpeg";
 
 export const banners = [
   {
