955293e86c8676631322a24d9709a5ec82e06cb4
Alexey <alexey@MacBook-Pro-2.local>
2024-10-23 17:30:17 +0300

    update telegram bot banner

--- src/constants/banners.ts
+++ src/constants/banners.ts
@@ -1,36 +1,36 @@
-import { DSA_URL, TELEGRAM_BOT_URL } from "../constants/links";
-import telegramBot from "../images/telegramBot.jpeg";
-import telegramBotMob from "../images/telegramBotMob.svg";
-import dsa from "../images/dsa.jpg";
-import dsaMob from "../images/dsaMob.svg";
-import hashex from "../images/HashExBanner.jpeg";
-import hashexMob from "../images/HashExBannerMob.jpeg";
-import hashexContacts from "../images/HashExContacts.jpg";
-import hashexContactsMob from "../images/HashExContactsMob.jpg";
+import { DSA_URL, TELEGRAM_BOT_URL } from '../constants/links'
+import telegramBot from '../images/telegramBot.jpg'
+import telegramBotMob from '../images/telegramBotMob.jpg'
+import dsa from '../images/dsa.jpg'
+import dsaMob from '../images/dsaMob.svg'
+import hashex from '../images/HashExBanner.jpeg'
+import hashexMob from '../images/HashExBannerMob.jpeg'
+import hashexContacts from '../images/HashExContacts.jpg'
+import hashexContactsMob from '../images/HashExContactsMob.jpg'
 
 export const banners = [
   {
-    alt: "Token Checker Telegram Bot",
+    alt: 'Token Checker Telegram Bot',
     src: telegramBot,
     link: TELEGRAM_BOT_URL,
     srcMob: telegramBotMob,
   },
   {
-    alt: "Defi Security Alliance",
+    alt: 'Defi Security Alliance',
     src: dsa,
     link: DSA_URL,
     srcMob: dsaMob,
   },
   {
-    alt: "HashEx",
+    alt: 'HashEx',
     src: hashex,
-    link: "https://hashex.org/smart-contract-audit/",
+    link: 'https://hashex.org/smart-contract-audit/',
     srcMob: hashexMob,
   },
   {
     alt: 'HashEx Contacts',
     src: hashexContacts,
     srcMob: hashexContactsMob,
-    link: "https://hashex.org/contacts/",
-  }
-];
+    link: 'https://hashex.org/contacts/',
+  },
+]
