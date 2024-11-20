6295acad866ec1917956ddeec1cf5679e5b3fb10
Alexey <alexey@MacBook-Pro-2.local>
2024-11-20 20:09:49 +0300

    update telegram bot link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,5 @@
-export const HASHEX_URL = "https://hashex.org";
-export const INSTRUCTIONS_URL = "https://github.com/HashEx/abiencoder#readme";
-export const TELEGRAM_BOT_URL = "https://telegram.me/CheckMyTokenBot";
-export const MAXIMIZER_URL = "https://app.analytex.today/maximizer";
-export const DSA_URL = "https://defisec.info/";
+export const HASHEX_URL = 'https://hashex.org'
+export const INSTRUCTIONS_URL = 'https://github.com/HashEx/abiencoder#readme'
+export const TELEGRAM_BOT_URL = 'https://t.me/hashex_auditor_bot'
+export const MAXIMIZER_URL = 'https://app.analytex.today/maximizer'
+export const DSA_URL = 'https://defisec.info/'
